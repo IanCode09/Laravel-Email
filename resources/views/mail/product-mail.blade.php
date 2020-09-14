36fec29de4a6012c499eacdfbd8d68ee70c60066
IanCode09 <ianlombu@protonmail.com>
2020-09-14 09:57:35 +0700

    Final Result

---
+++ resources/views/mail/product-mail.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('style/bootstrap.min.css')}}">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Selamat</h1>
+        <p>Anda telah membeli barang <strong>{{$product->product_name}}</strong></p>
+        <p>Thank You</p>
+        <strong>LazyProg</strong>
+    </div>
+
+    <script src="{{asset('scripts/jquery-3.5.0.min.js')}}"></script>
+    <script src="{{asset('scripts/bootstrap.min.js')}}"></script>
+</body>
+
+</html>
